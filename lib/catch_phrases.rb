a8543e2c93c34aaf9033b19242107a7ad9a22a71
Robert Obi <robobi97@googlemail.com>
2021-02-03 21:10:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,4 @@
-  status = "Thank You Mario! But our Princess is in Another Castle!"
+  $status = "Thank You Mario! But our Princess is in Another Castle!"
 def mario 
   phrase = "It's-a me, Mario!"
   puts phrase
