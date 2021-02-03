1378e996a2121ac8a385ccb60e2a0fddd5646743
Robert Obi <robobi97@googlemail.com>
2021-02-03 21:16:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,6 +5,6 @@ def mario
 end 
 
 def toadstool
-  status = "Thank You Mario! But Our Princess Is In Another Castle!"
-  puts status
-end 
+ status = "Thank You Mario! But our Princess Is In Another Castle!"
+ puts status
+end
