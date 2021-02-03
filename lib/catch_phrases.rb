62042df12e3d602486bf00014c7efd168b70d8bb
Robert Obi <robobi97@googlemail.com>
2021-02-03 21:10:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,9 @@
-def mario 
   status = "Thank You Mario! But our Princess is in Another Castle!"
+def mario 
   phrase = "It's-a me, Mario!"
   puts phrase
 end 
 
 def toadstool
-  status = "Thank you Mario! But out Princess is in Another Castle"
-  puts status
+puts status
 end 
